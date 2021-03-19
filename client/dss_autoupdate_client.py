ae84ff96899621cc40b1279c689a04ecb5e9cd6c
李赟 <laobaitu@gmail.com>
2021-03-19 08:37:56 +0800

    init frame

--- client/dss_autoupdate_client.py
+++ client/dss_autoupdate_client.py
@@ -2,14 +2,49 @@
 # -*- coding: utf-8 -*-
 
 import configparser
+import socket
+import json
+import logging
+import os
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+logging.basicConfig(level=logging.INFO)
 
-def RequireHeader():
+CONFIG_FILE='config.ini'
+
+request_data = {
+    'request' : None
+}
+
+def ReadConfig():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+    return config
+
+def InitSocket(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip, port))
+    if RequestHeader(sock):
+        RequestPackage(sock)
+    RequestExit(sock)
+    
+
+def RequestHeader(sock):
+    request_data['request'] = 'header'
+    sock.send(json.dumps(request_data))
+    return True
+
+def RequestPackage(sock):
+    request_data['request'] = 'package'
+    sock.send(json.dumps(request_data))
+    return
+
+def RequestExit(sock):
+    request_data['request'] = 'exit'
+    sock.send(json.dumps(request_data))
     pass
 
-def ConnectSession():
+if __name__=='__main__':
+    config = ReadConfig()
+    InitSocket(config['Client']['ip'], int(config['Client']['port']))
     pass
-
 
--- server/dss_autoupdate_server.py
+++ server/dss_autoupdate_server.py
@@ -4,17 +4,60 @@
 import configparser
 import socket
 import json
+import threading
+import logging
+import os
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+logging.basicConfig(level=logging.INFO)
 
-def InitSocket(ip, port):
+CONFIG_FILE='config.ini'
+
+def ReadConfig():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+    return config
+
+def InitSocket(ip, port, max_clients):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logging.info('Attempt to bind IP/Port: %s:%d' % (ip, port))
+    s.bind((ip, port))
+    s.listen(max_clients)
+    #while True:
+    # 接受一个新连接:
+    sock, addr = s.accept()
+    # 创建新线程来处理TCP连接:
+    t = threading.Thread(target=ProccessThread, args=(sock, addr))
+    t.start()
     pass
 
 def ProccessThread(sock, addr):
+    logging.info('Accept new connection from %s:%s...' % addr)
+    config = ReadConfig()
+    while True:
+        data = json.loads(sock.recv(1024))
+        if 'request' not in data:
+            continue
+        if data['request'] == 'header':
+            SendHeader(sock, config)
+        elif data['request'] == 'package':
+            SendPackage(sock, config)
+        elif data['request'] == 'exit':
+            sock.close()
+            break
+        else:
+            continue
     pass
 
-def SendHeader(sock):
+def SendHeader(sock, config):
+    logging.info('Send latest package header info: %s ...' % (config['Server']['latest']))
     pass
 
-def 
+def SendPackage(sock, config):
+    logging.info('Send latest package file: %s ...' % (config['Server']['latest']))
+    pass
+
+if __name__=='__main__':
+    config = ReadConfig()
+    InitSocket(config['Server']['ip'], int(config['Server']['port']), int(config['Server']['max_clients']))
+    pass
+
